a87b5a437eefc8e991576faa5f34e8e1b43e4993
Maciej Blim <maciej.blim@vistex.com>
2019-07-06 22:38:50 +0200

    cant get ref !!!

--- src/say_hi/core.cljs
+++ src/say_hi/core.cljs
@@ -1,16 +1,45 @@
 (ns say-hi.core
+  (:import goog.object)
   (:require [reagent.core :as r]))
 
 (enable-console-print!)
 
-(defonce state (r/atom {:text "Hello world!"}))
+(defonce state (r/atom {:employees [{:name "Maciek Blim" :position [23 45]}
+                                    {:name "Piotrek Łukomiak" :position [56 76]}]}))
 
-(defn hello-world []
-  [:div
-   [:h1 (:text @state)]
-   [:h3 "Edit this and watch it change!"]])
+(defn marker [[x y]]
+  [:div.marker {:style {:position "absolute"
+                        :top (str y "px")
+                        :left (str x "px")}}])
 
-(r/render-component [hello-world]
+(defn get-bcr [el]
+  (-> el
+      r/dom-node
+      .getBoundingClientRect))
+
+(defn office-plan [employees]
+  (let [!wrapper (r/atom nil)]
+    (prn @!wrapper) ;; something is no yes
+    [:div.office-plan-wrapper {:ref (fn [el]
+                                      (if-not (.is goog.object el @!wrapper) (reset! !wrapper el)))}
+     (if @!wrapper [(let [wrapper @!wrapper]
+                      (map (fn [emp]
+                             (let [pos (:position emp)
+                                   [pos-x pos-y] pos
+                                   bcr (get-bcr wrapper)
+                                   w (:width bcr)
+                                   h (:height bcr)
+                                   x (/ (* w pos-x) 100)
+                                   y (/ (* h pos-y) 100)]
+                               [marker [x y]])) employees))])]))
+
+(defn app []
+  (let [employees (get @state :employees)]
+    [:main
+     [:section.content [office-plan employees]]
+     [:aside.sidebar "List search projects itp .."]]))
+
+(r/render-component [app]
                     (. js/document (getElementById "app")))
 
 (defn on-js-reload []
