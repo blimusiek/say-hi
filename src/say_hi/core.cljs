ce0e740489adc3854cd225c7ff162406fca97193
Maciek Blim <blimusiek@users.noreply.github.com>
2019-07-22 22:43:03 +0200

    Feature/searchable list (#2)
    * list + search input
    
    * just wondering if search can be done this way
    
    * debounce is harder than expected
    
    * miracle -> debounce is working as expected
    
    * there is a list and its searchable -< I assume thats good

--- src/say_hi/core.cljs
+++ src/say_hi/core.cljs
@@ -1,16 +1,49 @@
 (ns say-hi.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [clojure.string :refer [blank?]]
+            [cljs.core.async :refer [chan sliding-buffer timeout tap mult offer!]]
+            [cljs.core.async :refer-macros [go go-loop alt!]]))
 
 (enable-console-print!)
 
-(defonce state (r/atom {:employees [{:name "Maciek Blim" :position [23 45] :project "Vizi"}
-                                    {:name "Piotrek Łukomiak" :position [56 76] :project "Vizi"}]}))
+(def employees [{:name "Maciek Blim" :position [23 45] :project "Vizi"}
+                {:name "Piotrek Łukomiak" :position [56 76] :project "Vizi"}])
+
+(def timeout-ms 400)
+
+;; It seems to be working
+;; Is it good? Is it ugly?
+;; Is it a proper way to solve that kind of problems in cljs/clj?
+(defn debounce
+  ([f]
+   (debounce f timeout-ms))
+  ([f ms]
+   (let [lock (chan (sliding-buffer 1))
+         stop (chan (sliding-buffer 1))]
+     (go-loop [prev-lock nil]
+       (when-let [new-lock (<! lock)]
+         (if prev-lock (offer! prev-lock :stop))
+         (recur new-lock)))
+     (fn [val]
+       (let [stop-chan (chan 1)]
+         (go
+           (offer! lock stop-chan)
+           (alt!
+             (timeout ms) ([] (do (prn val "resolved") (f val)))
+             (tap (mult stop) stop-chan) ([] (prn val "blocked")))))))))
 
 
 (defn marker-details [{name :name
                        project :project}]
   [:div.marker-details [:div name] [:div project]])
 
+(defn search-class-names [emp]
+  (let [is-in-search (contains? emp :found)]
+    (if is-in-search
+      (let [is-found (not (nil? (get emp :found)))]
+        (if is-found "searched-and-found" "searched-and-not-found")))))
+
+;; TODO animation of size on find
 (defn marker [{x :x y :y info :info}]
   (let [!is-mouse-over (r/atom false)]
     (fn []
@@ -24,6 +57,7 @@
                               :left left
                               :height size-px
                               :width size-px}
+                      :class (search-class-names info)
                       :on-mouse-over (fn [] (reset! !is-mouse-over true))
                       :on-mouse-out (fn [] (reset! !is-mouse-over false))}
          (if @!is-mouse-over [marker-details info])]))))
@@ -36,37 +70,57 @@
   (fn [el]
     (if (and el (not (identical? el @!ref))) (reset! !ref el))))
 
-(defn office-plan [employees]
+(defn office-plan []
   (let [!wrapper (r/atom nil)]
-    (fn []
+    (fn [employees]
       [:div.office-plan-wrapper {:ref (set-ref! !wrapper)}
        (if-let [wrapper @!wrapper]
          (for [emp employees]
-           (let [pos (:position emp)
-                 [pos-x pos-y] pos
+           (let [{:keys [name project] [pos-x pos-y] :position} emp
                  bcr (get-bcr wrapper)
                  w (.-width bcr)
                  h (.-height bcr)
                  x (/ (* w pos-x) 100)
-                 y (/ (* h pos-y) 100)
-                 name (:name emp)
-                 project (:project emp)]
-             ^{:key emp} [marker {:x x :y y :info {:name name
-                                                   :project project}}])))])))
+                 y (/ (* h pos-y) 100)]
+             ^{:key emp} [marker {:x x :y y :info emp}])))])))
 
+;; TODO sort over found
+;; TODO higlight match
+;; TODO click higlight coresponding marker
+(defn employee-list [employees]
+  [:div.employees-list (for [emp employees]
+                         (let [name (:name emp)
+                               project (:project emp)]
+                           ^{:key emp} [:div.employees-list-item {:class (search-class-names emp)} [:div.employees-list-item-label name]
+                                        [:div.employees-list-item-legend project]]))])
 
+(defn employee-search [on-search]
+  (let [on-change (debounce (fn [q] (on-search q)))]
+    (fn []
+      [:div.employee-search [:input {:type "text"
+                                     :on-change (fn [e] (on-change (-> e .-target .-value)))}]])))
 
-(defn app []
-  (let [employees (get @state :employees)]
-    [:main
-     [:section.content [office-plan employees]]
-     [:aside.sidebar "List search projects itp .."]]))
+(defn match-query [query {:keys [name project]}]
+  (let [pattern (re-pattern (str "(?i)" query))]
+    (or (re-find pattern name) (re-find pattern project))))
 
-(r/render-component [app]
+(defn mark-found [query emp]
+  (assoc emp :found (match-query query emp)))
+
+(defn search-query [query employees]
+  (map (fn [emp]
+         (mark-found query emp)) employees))
+
+(defn app [employees]
+  (let [!query (r/atom "")]
+    (fn []
+      (let [query @!query
+            emps (if (blank? query) employees (search-query query employees))
+            on-search! (fn [query] (reset! !query query))]
+        [:main
+         [:section.content [office-plan emps]]
+         [:aside.sidebar [employee-search on-search!] [employee-list emps]]]))))
+
+(r/render-component [app employees]
                     (. js/document (getElementById "app")))
-
-(defn on-js-reload [])
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
 
